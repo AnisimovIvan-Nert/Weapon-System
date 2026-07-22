b320b99ec2c7150528ff77bdd2606710320ca3d6
AnisimovIvan-Nert <anisimov.ivan.work@gmail.com>
2026-07-22 21:09:06 +0300

    MovingObjectOperation extract AbstractOrchestratorOperation abstraction

--- Weapon-System/Assets/Scripts/OperationSystem.Containers/Operations/MovingObjectOperation.cs
+++ Weapon-System/Assets/Scripts/OperationSystem.Containers/Operations/MovingObjectOperation.cs
@@ -1,39 +1,28 @@
 ﻿using System;
-using System.Collections;
-using System.Threading.Tasks;
-using Coroutine;
-using Coroutine.Instructions;
+using System.Collections.Generic;
 using OperationSystem.Containers.Units;
 using OperationSystem.Containers.Units.Containers;
 using OperationSystem.Handlers.Units;
-using OperationSystem.Operations;
+using OperationSystem.Operations.Orchestrator;
+using OperationSystem.Operations.Staged;
 using OperationSystem.Units;
 
 namespace OperationSystem.Containers.Operations
 {
-    public class MovingObjectOperation : AbstractOperation
+    public class MovingObjectOperation : AbstractOrchestratorOperation
     {
         private readonly IPlayer _executor;
         private readonly IUnit _target;
-        
+
         private readonly IUnitOperationHandler<IContainer> _sender;
         private readonly IUnitOperationHandler<IContainer> _receiver;
 
-        private SendObjectUnitOperation? _sendOperation;
-        private ReceiveObjectOperation? _receiveObjectOperation;
-
-        private SendObjectUnitOperation SendOperation => _sendOperation 
-                                                         ?? throw new InvalidOperationException();
-
-        private ReceiveObjectOperation ReceiveObjectOperation => _receiveObjectOperation 
-                                                                 ?? throw new InvalidOperationException();
-        
         public MovingObjectOperation(
             Guid identifier,
             IPlayer executor,
             IUnit target,
             IUnitOperationHandler<IContainer> sender,
-            IUnitOperationHandler<IContainer> receiver) 
+            IUnitOperationHandler<IContainer> receiver)
             : base(identifier)
         {
             _executor = executor;
@@ -42,114 +31,21 @@ namespace OperationSystem.Containers.Operations
             _receiver = receiver;
         }
 
-        public override void Increment(IOperationContext context)
+        protected override ICollection<IStagedOperation> GetOrchestratedOperations()
         {
-            _sendOperation ??= new SendObjectUnitOperation(Identifier, _executor, _target);
-            _receiveObjectOperation ??= new ReceiveObjectOperation(Identifier, _executor, _target);
+            var sendOperation = new SendObjectUnitOperation(Identifier, _executor, _target);
+            var receiveObjectOperation = new ReceiveObjectOperation(Identifier, _executor, _target);
+            return new IStagedOperation[] { sendOperation, receiveObjectOperation };
+        }
+
+        protected override void RunOrchestratedOperations(ICollection<IStagedOperation> operations)
+        {
+            var operationArray = (IStagedOperation[])operations;
+            var sendOperation = operationArray[0];
+            var receiveObjectOperation = operationArray[1];
             
-            Coroutine ??= IncrementEnumerator(context).ToCoroutine();
-
-            while (Coroutine.MoveNext())
-            {
-                if (Coroutine.InContinueState())
-                    continue;
-
-                return;
-            }
-
-            IsCompleted = true;
-            AppendException(Coroutine.Exception);
-
-            if (Exception == null)
-            {
-                _sendOperation.Complete();
-                _receiveObjectOperation.Complete();
-            }
-            else
-            {
-                _sendOperation.Cancel(Exception);
-                _receiveObjectOperation.Cancel(Exception);
-            }
-        }
-
-        protected override IEnumerator IncrementEnumerator(IOperationContext context)
-        {
-            SendOperation.RunOperation(_sender);
-            ReceiveObjectOperation.RunOperation(_receiver);
-
-            yield return Validate();
-            yield return AcquireLocks();
-            yield return RecordPossibleMutations();
-            yield return Execute();
-        }
-
-        private IEnumerator Validate()
-        {
-            yield return new WaitForTask(SendOperation.Validate());
-            yield return new WaitForTask(ReceiveObjectOperation.Validate());
-        }
-        
-        private IEnumerator AcquireLocks()
-        {
-            var timeout = AcquireLocksTimeout;
-
-            while (timeout > 0)
-            {
-                timeout--;
-
-                var success = false;
-                yield return Acquire().GetResult<bool>(result => success |= result);
-                
-                if (success)
-                    yield break;
-
-                yield return new WaitForTask(SendOperation.ReleaseLocks());
-                yield return new WaitForTask(ReceiveObjectOperation.ReleaseLocks());
-            }
-
-            throw new AcquireException();
-
-            IEnumerator Acquire()
-            {
-                var result = true;
-                yield return HandleTaskResult(SendOperation.TryAcquireLocks()).GetResult<bool>(o => result &= o);
-                
-                if (result)
-                    yield return HandleTaskResult(ReceiveObjectOperation.TryAcquireLocks()).GetResult<bool>(o => result &= o);
-
-                yield return result;
-                yield break;
-                
-
-                IEnumerator HandleTaskResult(Task task)
-                {
-                    var catchException = new CatchException(new WaitForTask(task));
-                    yield return catchException;
-
-                    if (catchException.Exception is AcquireException)
-                    {
-                        yield return false;
-                        yield break;
-                    }
-
-                    if (catchException.Exception != null)
-                        throw catchException.Exception;
-
-                    yield return true;
-                }
-            }
-        }
-        
-        private IEnumerator RecordPossibleMutations()
-        {
-            yield return new WaitForTask(SendOperation.RecordPossibleMutations());
-            yield return new WaitForTask(ReceiveObjectOperation.RecordPossibleMutations());
-        }
-        
-        private IEnumerator Execute()
-        {
-            yield return new WaitForTask(SendOperation.Execute());
-            yield return new WaitForTask(ReceiveObjectOperation.Execute());
+            sendOperation.RunOperation(_sender);
+            receiveObjectOperation.RunOperation(_receiver);
         }
     }
 }
---
+++ Weapon-System/Assets/Scripts/OperationSystem/Operations/Orchestrator/AbstractOrchestratorOperation.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Coroutine;
+using Coroutine.Instructions;
+using OperationSystem.Operations.Staged;
+
+namespace OperationSystem.Operations.Orchestrator
+{
+    public abstract class AbstractOrchestratorOperation : AbstractOperation
+    {
+        private ICollection<IStagedOperation>? _operations;
+
+        private ICollection<IStagedOperation> Operations => _operations ?? throw new InvalidOperationException();
+
+        protected AbstractOrchestratorOperation(Guid identifier) 
+            : base(identifier)
+        {
+        }
+
+        public override void Increment(IOperationContext context)
+        {
+            _operations ??= GetOrchestratedOperations();
+            
+            Coroutine ??= IncrementEnumerator(context).ToCoroutine();
+
+            while (Coroutine.MoveNext())
+            {
+                if (Coroutine.InContinueState())
+                    continue;
+
+                return;
+            }
+
+            IsCompleted = true;
+            AppendException(Coroutine.Exception);
+
+            foreach (var operation in Operations)
+            {
+                if (Exception == null)
+                    operation.Complete();
+                else
+                    operation.Cancel(Exception);
+            }
+        }
+
+        protected abstract ICollection<IStagedOperation> GetOrchestratedOperations();
+        protected abstract void RunOrchestratedOperations(ICollection<IStagedOperation> operations);
+        
+        protected override IEnumerator IncrementEnumerator(IOperationContext context)
+        {
+            RunOrchestratedOperations(Operations);
+
+            yield return Validate();
+            yield return AcquireLocks();
+            yield return RecordPossibleMutations();
+            yield return Execute();
+        }
+        
+        protected virtual IEnumerator Validate()
+        {
+            foreach (var operation in Operations)
+                yield return new WaitForTask(operation.Validate());
+        }
+        
+        protected virtual IEnumerator AcquireLocks()
+        {
+            var timeout = AcquireLocksTimeout;
+            while (timeout-- > 0)
+            {
+                var success = false;
+                yield return TryAcquireLocks().GetResult<bool>(result => success |= result);
+                if (success)
+                    yield break;
+
+                yield return ReleaseResources();
+                yield return null;
+            }
+
+            throw new AcquireException();
+        }
+        
+        protected virtual IEnumerator RecordPossibleMutations()
+        {
+            foreach (var operation in Operations)
+                yield return new WaitForTask(operation.RecordPossibleMutations());
+        }
+        
+        protected virtual IEnumerator Execute()
+        {
+            foreach (var operation in Operations)
+                yield return new WaitForTask(operation.Execute());
+        }
+        
+        protected virtual IEnumerator ReleaseResources()
+        {
+            foreach (var operation in Operations)
+                yield return new WaitForTask(operation.ReleaseLocks());
+        }
+        
+        protected virtual IEnumerator TryAcquireLocks()
+        {
+            var result = true;
+            foreach (var operation in Operations)
+            {
+                yield return HandleTaskResult(operation.TryAcquireLocks())
+                    .GetResult<bool>(o => result &= o);
+                    
+                if (!result)
+                    break;
+            }
+
+            yield return result;
+            yield break;
+
+            IEnumerator HandleTaskResult(Task task)
+            {
+                var catchException = new CatchException(new WaitForTask(task));
+                yield return catchException;
+
+                if (catchException.Exception is AcquireException)
+                {
+                    yield return false;
+                    yield break;
+                }
+
+                if (catchException.Exception != null)
+                    throw catchException.Exception;
+
+                yield return true;
+            }
+        }
+    }
+}
